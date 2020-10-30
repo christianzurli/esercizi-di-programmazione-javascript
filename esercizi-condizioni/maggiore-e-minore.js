35d87d7c95206a35f528ab96acafa1734952eca1
christianzurli <73234342+christianzurli@users.noreply.github.com>
2020-10-30 13:11:26 +0100

    Verifica

--- esercizi-condizioni/maggiore-e-minore.js
+++ esercizi-condizioni/maggiore-e-minore.js
@@ -10,3 +10,39 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var a, b, c, d, min, max
+a = 3, b = -1, c = 4, d = -2
+
+max = a
+if (b > max) {
+    max = b
+}
+if (c > max) {
+    max = c
+}
+if (d > max) {
+    max = d
+}
+
+min = a
+if (b < min) {
+    min = b
+}
+if (c < min) {
+    min = c
+}
+if (d < min) {
+    min = d
+}
+
+/*
+if ((b > max) && (b>c) && (b>d)) max = b;
+if ((c>max) && (c>b) && (c>d)) max = c;
+if ((d>max) && (d>b) && (d>b)) max = d;
+
+if ((b<min) && (b<c) && (b<c)) min = b;
+if ((c<min) && (c<b) && (c<d)) min = c;
+if ((d<min) && (d<b) && (d<c)) min = d;
+*/
+console.log('maggiore: ' + max + ', minore: ' + min)
--- esercizi-condizioni/menu-dei-dolci.js
+++ esercizi-condizioni/menu-dei-dolci.js
@@ -21,3 +21,4 @@
 
   http://www.imparareaprogrammare.it
 */
+var input = 4
